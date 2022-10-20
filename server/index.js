a0ba1c63096371afefb68eba4aa20164a1f9df8d
uchechukwu81 <blessinguche96@gmail.com>
2022-10-20 10:52:23 +0100

    client & server folders

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const UserModel = require("./models/Users");
+
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(
+    "mongodb+srv://mongo:Uchechukwu%4081@cluster0.ugxmrkg.mongodb.net/merntutorial?retryWrites=true&w=majority"
+)
+
+app.get("/getUsers", (req, res) => {
+    UserModel.find({}, (err, result) => {
+        if (err){
+            res.json(err);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+app.post("/createUser", async(req, res) => {
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save();
+
+    res.json(user)
+});
+
+app.listen(3000, () => {
+    console.log("Server runs perfectly!");
+})
